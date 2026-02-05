13fcdabe7074a29a4a5d89346decf62ff60adc0d
BkStyle89 <kajzer@hotmail.it>
2026-02-05 12:39:50 +0100

    correzione in classe

--- snack-3/3/script.js
+++ snack-3/3/script.js
@@ -1,12 +1,33 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
 
-const numbers =[]
+const numbers = []
 
 var sum = 0
 
 for(i=0; i<=10;i++){
     let numbers = prompt ('scegli 10 numeri diversi');
-    sum += numbers[i];
+    sum.numbers=numbers
+    
 }
 console.log(sum)
+console.log(numbers)
+
+
+
+/* 
+correzione 
+
+let sum = 0
+
+for(i=0; i<10;i++){
+    let numbers = prompt ('scegli 10 numeri diversi');
+    console.log(numb);
+    sum=sum+numb
+}
+    
+console.log(sum)
+
+
+
+*/
